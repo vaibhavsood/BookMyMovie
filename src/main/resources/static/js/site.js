5ca24866b0766933d9aaa45e0f2eb79c3ed4c8b2
Vaibhav Sood <vaibhavsood12@gmail.com>
2018-11-22 16:25:56 +0530

    Improve seat booking flow after listing screenings

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -46,10 +46,23 @@ function bookTickets() {
     window.location.href = redirectLink;
 }
 
+var popoverContent = `<form action="/seats" method="get">
+                            <button class="btn btn-primary btn-sm" type="submit" name="count" value="1">1</button>
+                            <button class="btn btn-primary btn-sm" type="submit" name="count" value="2">2</button>
+                            <button class="btn btn-primary btn-sm" type="submit" name="count" value="3">3</button>
+                            <button class="btn btn-primary btn-sm" type="submit" name="count" value="4">4</button>
+                            <button class="btn btn-primary btn-sm" type="submit" name="count" value="5">5</button>
+                      </form>`;
+
 $(document).ready(function(){
     setPicker();
     setInitialDate();
-
+    $('[data-toggle="popover"]').popover({
+                                             container: 'body',
+                                             html:true,
+                                             animation: true,
+                                             content: popoverContent
+                                           });
     //$(".bookButton").click(function() {
 
     //}
